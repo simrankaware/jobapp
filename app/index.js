c7efc4ceedc1f2d53918a17229b4d4ae7d3878d4
Simran Kaware <simrankaware@Simrans-MacBook-Pro.local>
2023-08-31 12:48:06 +0100

    Added files

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default function Page() {
+  return (
+    <Home />
+  );
+}
+
